55706a04f2f273a95f0f3c351244dc82b6cd38b6
Aron Jones <aron.jones@gmail.com>
2020-03-03 11:47:53 -0500

    print webhook secret

--- pages/api/user_created.ts
+++ pages/api/user_created.ts
@@ -2,5 +2,6 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 export default function(req: NextApiRequest, res: NextApiResponse) {
   console.log('user created')
+  console.log('webhook secret', req.headers['x-hasura-webhook-secret'])
   res.send('okay')
 }
